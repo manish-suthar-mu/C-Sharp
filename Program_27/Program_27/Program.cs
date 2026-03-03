d14c65abd40f6ab90584f4361a668109c6f2320b
manish-suthar-mu <githubmu@gmail.com>
2026-03-03 08:32:46 +0530

    27 and 28

---
+++ Program_27/Program_27/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[8];
+        int pcount = 0;
+        int ncount = 0;
+
+        Console.WriteLine("Enter 8 numbers:");
+
+        for (int i = 0; i < 8; i++)
+        {
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            if (numbers[i] > 0)
+            {
+                pcount++;
+            }
+            else if (numbers[i] < 0)
+            {
+                ncount++;
+            }
+        }
+
+        Console.WriteLine("Positive numbers: " + pcount);
+        Console.WriteLine("Negative numbers: " + ncount);
+    }
+}
