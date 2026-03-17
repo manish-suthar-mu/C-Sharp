d2e7f9294bdc1f6e887c4aedc95941d3aa102395
manish-suthar-mu <githubmu@gmail.com>
2026-03-17 08:13:26 +0530

    program_31

---
+++ Program_31/Program_31/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Employee
+{
+    int employeeId;
+    string name;
+    int salary;
+    public Employee()
+    {
+        employeeId = 0;
+        name = "Unknown";
+        salary = 0;
+    }
+
+    public Employee(int id, string n, int sal)
+    {
+        employeeId = id;
+        name = n;
+        salary = sal;
+    }
+    public double CalSalary()
+    {
+        return salary * 12;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Employee ID: " + employeeId);
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Monthly Salary: " + salary);
+        Console.WriteLine("Annual Salary: " + CalSalary());
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee e1 = new Employee();
+        e1.Display();
+        Console.WriteLine();
+        Employee e2 = new Employee(101, "Manish", 25000);
+        e2.Display();
+    }
+}
