16768cccb7ca5eaf7884b243580b8297b8e9b349
manish-suthar-mu <githubmu@gmail.com>
2026-03-31 07:51:48 +0530

    program_33

---
+++ Program_33/Program_33/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+interface IResult
+{
+    bool Pass(int mark);
+}
+
+interface IDivision
+{
+    string Division(int average);
+}
+
+class Student : IResult, IDivision
+{
+    public bool Pass(int mark)
+    {
+        if (mark >= 50)
+            return true;
+        else
+            return false;
+    }
+
+    public string Division(int average)
+    {
+        if (average >= 60)
+            return "First Division";
+        else if (average >= 50)
+            return "Second Division";
+        else if (average >= 40)
+            return "Third Division";
+        else
+            return "Fail";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student();
+
+        int marks = 55;
+        int avg = 58;
+
+        Console.WriteLine("Pass Status: " + s.Pass(marks));
+        Console.WriteLine("Division: " + s.Division(avg));
+    }
+}
