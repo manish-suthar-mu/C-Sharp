77c065e20dd742d630cb0f78dc714efb82a77839
manish-suthar-mu <githubmu@gmail.com>
2026-04-07 08:00:53 +0530

    Program_35

---
+++ Program_35/Program_35/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+
+class Error
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int a = 10;
+            int b = 0;
+            int c = a / b;
+            Console.WriteLine(c);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error : ZeroDivisionError: division by zero");
+        }
+        finally
+        {
+            Console.WriteLine("Error : The use of try, catch and finally in C#");
+        }
+    }
+}
