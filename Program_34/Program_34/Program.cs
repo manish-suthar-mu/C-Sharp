3ed555c2316f413f0f76991181e3690be8f8b361
manish-suthar-mu <githubmu@gmail.com>
2026-04-07 07:51:52 +0530

    pro_34

---
+++ Program_34/Program_34/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Error
+{
+    static void Main(String[] args)
+    {
+        try
+        {
+            int a = 10;
+            int b = 0;
+            int c = a / b;
+            Console.WriteLine(c);
+        }
+        catch (Exception e) 
+        { 
+            Console.WriteLine("Error : ZeroDivisionError: division by zero....");
+        }
+    }
+}
